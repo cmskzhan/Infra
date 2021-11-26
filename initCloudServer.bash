9928509a93f94473848a313c5f8806b21c0ce5b3
cmskzhan <kaizhang@hotmail.com>
2021-11-26 10:50:41 +0000

    none interactive

--- initCloudServer.bash
+++ initCloudServer.bash
@@ -3,14 +3,14 @@ echo "make sure you have sudo permission!"
 echo "1. update yum"
   sudo yum install -y yum-utils
   sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-  sudo yum update
-  sudo yum upgrade
+  sudo yum -y update
+  sudo yum -y upgrade
 echo "2. upgrade python"
-  sudo dnf groupinstall 'development tools'
-  sudo yum remove python3  #old 3.6 default version
-  sudo yum install python3.9
+  sudo dnf groupinstall -y 'development tools'
+  sudo yum remove -y python3  #old 3.6 default version
+  sudo yum install -y python3.9
 echo "3. install docker"
-  sudo yum install docker-ce docker-ce-cli containerd.io --allowerasing
+  sudo yum install -y docker-ce docker-ce-cli containerd.io --allowerasing
   sudo systemctl start docker
   sudo systemctl enable docker
   sudo usermod -aG docker $USER
