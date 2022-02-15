755c42a5ff08f4c1c8652147713d4977752f0513
cmskzhan <kaizhang@hotmail.com>
2022-02-15 16:32:23 +0000

    add a comment to prompt for solution for docker

--- ubuntu2004.bash
+++ ubuntu2004.bash
@@ -9,3 +9,4 @@ echo "2. install aws cli"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+echo "if docker permission denied, check docker.sock permission, cloud_user group and logout/login"
