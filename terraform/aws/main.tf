6f43d6aba07d3e8c6d17aae5d9bf67f0e10a4ff5
kaizhang <kaizhang@hotmail.com>
2023-05-25 22:51:56 +0100

    added an incomplete ec2 streamlit

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -88,8 +88,8 @@ resource "aws_instance" "silioSync" {
     sudo echo "${var.aws_access_key}:${var.aws_secret_key}" > /home/ec2-user/.passwd-s3fs
     sudo chmod 600 /home/ec2-user/.passwd-s3fs
     sudo s3fs ${var.bucket_name} /mnt/s3 -o passwd_file=/home/ec2-user/.passwd-s3fs,nonempty,rw,allow_other,mp_umask=002,uid=1000,gid=1000
-    sudo echo "test test" > /mnt/s3/test.txt
-    sudo docker run -d --name silio-sync -p 8888:8888 -p 55555:55555 --restart always resilio/sync
+    sleep 30
+    sudo docker run -d --name silio-sync -p 8888:8888 -p 55555:55555 -v /mnt/s3:/mnt/sync --restart always resilio/sync
   EOF
 
   tags = {
@@ -98,8 +98,27 @@ resource "aws_instance" "silioSync" {
   security_groups = [aws_security_group.resilio-web.name] # not aws_security_group.resilio-web.id
 }
 
+resource "aws_instance" "streamlit" {
+  ami = var.ami_freetier["rhel"]
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.tf_test_key.key_name
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo amazon-linux-extras install docker -y
+    sudo systemctl start docker
+    sudo usermod -a -G docker ec2-user
+    sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+  EOF
 
+  tags = {
+    Name = "A simple streamlit app"
+  }
+  security_groups = [aws_security_group.resilio-web.name] # not aws_security_group.resilio-web.id
+}
 
 output "ssh_command" {
-  value = "ssh ec2-user@${aws_instance.silioSync.public_ip}"
+  value = "ssh ec2-user@${aws_instance.streamlit.public_ip}"
 }
