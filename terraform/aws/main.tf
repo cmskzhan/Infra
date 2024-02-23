51372fa4a9cc8fc70a5f74901839c16d10b212f2
kai <kai@wsl2Pc.ai>
2024-02-23 21:38:51 +0000

    amazon-linux-extras: command not found

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -105,7 +105,7 @@ resource "aws_instance" "streamlit" {
   user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y
-    sudo amazon-linux-extras install docker -y
+    sudo yum install docker git -y
     sudo systemctl start docker
     sudo usermod -a -G docker ec2-user
     sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
