33866359fdca03d945cd51dc8760d0ea218265ee
kai <kai@wsl2Pc.ai>
2024-04-21 19:29:25 +0100

    github action

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -106,11 +106,12 @@ resource "aws_instance" "streamlit" {
     #!/bin/bash
     sudo yum update -y
     sudo yum install docker git -y
-    sudo systemctl start docker
-    sudo usermod -a -G docker ec2-user
+    # sudo systemctl start docker
+    # sudo usermod -a -G docker ec2-user
     sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    git clone https://github.com/cmskzhan/helloworld.git /home/ec2-user/github
   EOF
 
   tags = {
